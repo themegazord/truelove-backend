c124cacde0e7315c3e2c7e404b5b3e3db3dae4ab
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-09-19 21:43:58 -0400

    add: modelo Categoria com relações de produtos e uso de Ulids

---
+++ app/Models/Categoria.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Categoria extends Model
+{
+  use HasUlids;
+
+  protected $table = 'categorias';
+  protected $fillable = ['pai_id', 'slug', 'nome', 'descricao'];
+
+  public function produtos(): BelongsToMany
+  {
+    return $this->belongsToMany(Produto::class, 'categoria_produto');
+  }
+}
